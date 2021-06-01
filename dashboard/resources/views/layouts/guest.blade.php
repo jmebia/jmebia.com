56464f31301a05e4c1957e8268ae5bced82cbfb8
jmebia <jmebia@gmail.com>
2021-06-01 22:14:51 +0800

    adjusted fb plugin

--- dashboard/resources/views/layouts/guest.blade.php
+++ dashboard/resources/views/layouts/guest.blade.php
@@ -34,6 +34,13 @@
             {{ $slot }}
         </div>
 
+        <!-- Messenger Chat Plugin div -->
+        <div id="fb-root"></div>
+
+        <!-- Your Chat Plugin div -->
+        <div id="fb-customer-chat" class="fb-customerchat">
+        </div>
+
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}"></script>
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
@@ -42,31 +49,25 @@
              @yield('scripts')
         @endif
 
-        <!-- Messenger Chat Plugin Code -->
-        <div id="fb-root"></div>
-
-        <!-- Your Messenger Chat Plugin code -->
-        <div id="fb-customer-chat" class="fb-customerchat">
-        </div>
-
+        <!-- Your Messenger Chat Plugin script -->
         <script>
-        var chatbox = document.getElementById('fb-customer-chat');
-        chatbox.setAttribute("page_id", "104616258507894");
-        chatbox.setAttribute("attribution", "biz_inbox");
-        window.fbAsyncInit = function() {
-            FB.init({
-            xfbml            : true,
-            version          : 'v10.0'
-            });
-        };
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "104616258507894");
+            chatbox.setAttribute("attribution", "biz_inbox");
+            window.fbAsyncInit = function() {
+                FB.init({
+                xfbml            : true,
+                version          : 'v10.0'
+                });
+            };
 
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
         </script>
     </body>
 </html>
